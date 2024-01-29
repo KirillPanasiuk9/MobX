90af754d12d84f9c4d06759c6c1b74ef27c6eb0b
Kirill Panasiuk <101974969+KirillPanasiuk9@users.noreply.github.com>
2024-01-29 23:08:58 +0300

    Add error handling (#5)

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { RouterProvider } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { theme } from "./theme/theme";
 import { appRouter } from "./router/appRouter";
+import { ErrorHandlingProvider } from "./errorHandling";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <RouterProvider router={appRouter} />
+      <ErrorHandlingProvider>
+        <CssBaseline />
+        <RouterProvider router={appRouter} />
+      </ErrorHandlingProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
